f06d98fd08c2a16c71f0b3a9197cc91da5d2dfba
HavensurgeSW <matias@karplus.com.ar>
2022-03-16 23:44:22 -0300

    Added planetary rotations and orbit

---
+++ HSS-DVJ2-Framework/HSS-DVJ2-Framework/Assets/Scripts/EarthRotate.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthRotate : MonoBehaviour
+{
+    public GameObject sun;
+    public float speed;
+ 
+    // Update is called once per frame
+    void Update() 
+    {
+        //Earth standalone day rotation
+        transform.Rotate(0,-0.1f,0);
+
+        //Orbit
+        transform.RotateAround(sun.transform.position, new Vector3(0,-1,0), 6*Time.deltaTime);
+    }
+}
