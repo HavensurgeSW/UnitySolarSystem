d1cdadad90e8895067240681c94f019997c4a171
HavensurgeSW <matias@karplus.com.ar>
2022-03-21 23:07:32 -0300

    Added camera movement and Earth tracking

--- HSS-DVJ2-Framework/HSS-DVJ2-Framework/Assets/Scripts/CamMovement.cs
+++ HSS-DVJ2-Framework/HSS-DVJ2-Framework/Assets/Scripts/CamMovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class CamMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+ //GENERIC
+    public GameObject cam;
+    public float speed = 10;
+    public Vector2 motion;
+    public bool planetLock = false;
+    private Vector3 OGPos;
+ // PlanetLock
+     public Transform planetTransform;
+    private Vector3 _camOffset;
+    public float SmoothFactor = 0.5f;
+
     void Start()
     {
-        
+        _camOffset = transform.position - planetTransform.position;
+        OGPos = transform.position;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            if(!planetLock){
+                planetLock = true;
+            }else{
+                planetLock = false;
+                transform.position = OGPos;
+            }
+        }
+
+        if(planetLock){
+            Vector3 newPos = planetTransform.position +_camOffset;
+            transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+        }else{
+            motion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            cam.transform.Translate(motion * speed * Time.deltaTime);
+        }
+
+
     }
 }
